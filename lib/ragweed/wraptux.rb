ee680a23dffb192abe48de09e4defea91966e288
tduehr <td@matasano.com>
2009-09-21 17:17:31 -0500

    require \'ragweed/[rasm|wrap32|wraptux|wraposx]\' work now on their own

--- lib/ragweed/wraptux.rb
+++ lib/ragweed/wraptux.rb
@@ -32,12 +32,18 @@ module Ragweed::Wraptux
     args.empty? ? PATH : ::File.join(PATH, args.flatten)
   end
 
+  # Utility function to load utility classes and extensions
+  def self.require_utils
+    %w{utils}.each{|r| require self.libpath(r)+'.rb'}
+  end
+
   # Utility method used to require all files ending in .rb that lie in the
   # directory below this file that has the same name as the filename passed
   # in. Optionally, a specific _directory_ name can be passed in such that
   # the _filename_ does not have to be equivalent to the directory.
   #
   def self.require_all_libs_relative_to( fname, dir = nil )
+    self.require_utils
     dir ||= ::File.basename(fname, '.*')
     search_me = ::File.expand_path(
         ::File.join(::File.dirname(fname), dir, '**', '*.rb'))
