012102f754efdf8999f84f4744f9f9fface908e4
Chris <chris.rohlf@gmail.com>
2011-03-26 00:14:38 -0400

    add other memory protection constants

--- lib/ragweed/wrap32/debugging.rb
+++ lib/ragweed/wrap32/debugging.rb
@@ -14,7 +14,14 @@ module Ragweed::Wrap32
   end
 
   module PagePermissions
+    PAGE_NOACCESS = 0x1
+    PAGE_READONLY = 0x2
+    PAGE_READWRITE = 0x4
+    PAGE_WRITECOPY = 0x8
+    PAGE_EXECUTE = 0x10
+    PAGE_EXECUTE_READ = 0x20
     PAGE_EXECUTE_READWRITE = 0x40
+    PAGE_EXECUTE_WRITECOPY = 0x80
   end
 
   module ExceptionCodes
